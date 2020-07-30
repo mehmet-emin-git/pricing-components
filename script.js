88b831d08e47572bd29c417ccc8af9e2036b5788
mechitoua mohammed amine <mechitoua.m.amine@gmail.com>
2020-07-30 17:20:04 +0100

    styling the pill switcher

---
+++ script.js
@@ -0,0 +1,14 @@
+const year = document.querySelectorAll(".year");
+const month = document.querySelectorAll(".month");
+const pill = document.querySelector(".pill");
+const input = document.querySelector(".check");
+
+input.addEventListener("change", () => {
+  for (let item of year) {
+    item.classList.toggle("show");
+  }
+
+  for (let item of month) {
+    item.classList.toggle("show");
+  }
+});
